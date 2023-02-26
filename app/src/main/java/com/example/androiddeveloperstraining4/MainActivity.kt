a9457c90f8923eb6a8fa5626d1326798fb235f5c
kiyo <i.mitsuki19@gmail.com>
2023-02-26 10:05:27 +0900

    書き換え元のThreadコードを追加

--- app/src/main/java/com/example/androiddeveloperstraining4/MainActivity.kt
+++ app/src/main/java/com/example/androiddeveloperstraining4/MainActivity.kt
@@ -20,6 +20,20 @@ class MainActivity : AppCompatActivity() {
 //        useThread()
 //        useCoroutine()
 //        useJob()
+        rewrite()
+    }
+
+    private fun rewrite() {
+        val states = arrayOf("Starting", "Doing Task 1", "Doing Task 2", "Ending")
+        repeat(3) {
+            Thread {
+                println("${Thread.currentThread()} has started")
+                for (i in states) {
+                    println("${Thread.currentThread()} - $i")
+                    Thread.sleep(50)
+                }
+            }.start()
+        }
     }
 
     /**
